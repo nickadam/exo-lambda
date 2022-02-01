bf27cc849966e1ac0c78e1b5c07d6252b6e2eb43
Nick Vissari <networknerd@gmail.com>
2022-02-01 13:33:58 -0500

    Add example script to test output

---
+++ example_script.ps1
@@ -0,0 +1,9 @@
+"your args are below"
+$args
+"this is just an example"
+"this is another example"
+Write-Warning "This is a warning"
+Write-Warning "This is another warning"
+Write-Error "This is an error"
+Write-Error "This is another error"
+Throw "I have thrown an error"
